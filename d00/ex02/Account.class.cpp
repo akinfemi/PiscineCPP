1e14147a7bbcd1ea65f25c0d7d4b7a1cae812d56
akinfemi <akinfemi.aluko@gmail.com>
2018-01-09 22:06:19 -0800

    final?

--- d00/ex02/Account.class.cpp
+++ d00/ex02/Account.class.cpp
@@ -11,20 +11,34 @@
 /* ************************************************************************** */
 
 #include "Account.class.hpp"
+#include <ctime>
+#include <iostream>
+#include <iomanip>
 
-// Account(void){
+Account::Account(void){
 
-// }
+}
+
 Account::Account(int initial_deposit){
     Account::_nbDeposits++;
     Account::_totalAmount += initial_deposit;
-    this->_nbDeposits = 1;
-    this->_accountIndex = 0;
+    this->_nbDeposits = 0;
+    this->_accountIndex = Account::_nbAccounts;
 	this->_amount = initial_deposit;
 	this->_nbWithdrawals = 0;
+    Account::_nbAccounts++;
+
+    Account::_displayTimestamp();
+    std::cout << "index:" << this->_accountIndex << ";"
+    << "amount:" << this->_amount << ";"
+    << "created" << std::endl;
 }
 
 Account::~Account(void){
+    Account::_displayTimestamp();
+    std::cout << "index:" << this->_accountIndex << ";"
+    << "amount:" << this->_amount << ";"
+    << "closed" << std::endl;
     return;
 }
 
@@ -49,25 +63,54 @@ int Account::getNbWithdrawals(void)
 
 void Account::displayAccountsInfos(void)
 {
-
+    Account::_displayTimestamp();
+    std::cout << "accounts:" << Account::_nbAccounts << ";"
+    << "total:" << Account::_totalAmount << ";"
+    << "deposits:" << Account::_totalNbDeposits << ";"
+    << "withdrawals:" << Account::_totalNbWithdrawals
+    << std::endl;
 }
 
 void    Account::makeDeposit(int deposit)
 {
+
+    Account::_displayTimestamp();
+    std::cout << "index:" << this->_accountIndex << ";"
+    << "p_amount:" << this->_amount << ";"
+    << "deposits:" << deposit << ";";
     Account::_totalAmount += deposit;
-    this->_totalAmount += deposit;
+    Account::_totalNbDeposits++;
+    this->_amount += deposit;
+    this->_nbDeposits++;
+    std::cout << "amount:" << this->_amount << ";"
+    << "nb_Deposits:" << this->_nbDeposits
+    << std::endl;
 }
 
 bool    Account::makeWithdrawal(int withdrawal)
 {
-    if (this->_totalAmount >= withdrawal)
+    if (this->_amount >= withdrawal)
     {
-        this->_totalAmount -= withdrawal;
+
+        Account::_displayTimestamp();
+        std::cout << "index:" << this->_accountIndex << ";"
+        << "p_amount:" << this->_amount << ";"
+        << "withdrawal:" << withdrawal << ";";
+        this->_amount -= withdrawal;
         this->_nbWithdrawals++;
         Account::_totalAmount -= withdrawal;
         Account::_totalNbWithdrawals++;
+        std::cout << "amount:" << this->_amount << ";"
+        << "nb_withdrawals:" << this->_nbWithdrawals
+        << std::endl;
+        // index:1;p_amount:819;withdrawal:34;amount:785;nb_withdrawals:1
         return true;
     }
+    Account::_displayTimestamp();
+    std::cout << "index:" << this->_accountIndex << ";"
+    << "p_amount:" << this->_amount << ";"
+    << "withdrawals:refused"
+    << std::endl;
     return false;
 }
 
@@ -76,15 +119,27 @@ int     Account::checkAmount(void) const {
 }
 
 void    Account::displayStatus(void) const{
-    
+    Account::_displayTimestamp();
+    std::cout << "index:" << this->_accountIndex << ";"
+    << "amount:" << this->_amount << ";"
+    << "deposits:" << this->_nbDeposits << ";"
+    << "withdrawals:" << this->_nbWithdrawals
+    << std::endl;
 }
 
 void Account::_displayTimestamp(void)
 {
-
+    time_t t = time(0);
+    struct tm * now = localtime(&t);
+    std::cout << "[" << (now->tm_year + 1900) 
+    << std::setw(2) << std::setfill('0') << (now->tm_mon + 1) 
+    << std::setw(2) << std::setfill('0') << now->tm_mday << '_' 
+    << std::setw(2) << std::setfill('0') << now->tm_hour
+    << std::setw(2) << std::setfill('0') << now->tm_min
+    << std::setw(2) << std::setfill('0') << now->tm_sec << "] ";
 }
 
-int	_nbAccounts = 0;
-int	_totalAmount = 0;
-int	_totalNbDeposits = 0;
-int	_totalNbWithdrawals = 0;
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
